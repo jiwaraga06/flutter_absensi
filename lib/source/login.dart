73ee8185ef2cf49ef0bb7f44a28840ec7faab5a4
jiwaraga06 <dermawanraga066@gmail.com>
2022-04-15 18:55:36 +0700

    Update baru

--- lib/source/login.dart
+++ lib/source/login.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_absensi/source/keluar.dart';
 import 'package:flutter_absensi/source/masuk.dart';
+import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:time_range/time_range.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -15,16 +17,25 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final formKey = GlobalKey<FormState>();
   TextEditingController controllerTerminal = TextEditingController();
-  TextEditingController controllerWaktu = TextEditingController();
+  TextEditingController controllerWaktuAwal = TextEditingController();
+  TextEditingController controllerWaktuAkhir = TextEditingController();
   var list = [];
+  var data = [];
   int status = 0;
   void submit() async {
     if (formKey.currentState!.validate()) {
+      list.add({
+        "terminal": controllerTerminal.text,
+        "waktuAwal": controllerWaktuAwal.text,
+        "waktuAkhir": controllerWaktuAkhir.text,
+        "status": status,
+      });
+      print(list);
       SharedPreferences pref = await SharedPreferences.getInstance();
       pref.setString('terminal', controllerTerminal.text);
       pref.setString('status', status.toString());
-        Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (_) => Masuk()));
+      pref.setString('setting', jsonEncode(list));
+      // Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => Masuk()));
       // if (status == 0) {
       // } else {
       //   Navigator.pushReplacement(
@@ -33,7 +44,7 @@ class _LoginState extends State<Login> {
     }
   }
 
-  var time;
+  var timeAwal;
   void selectTime() async {
     final TimeOfDay? newTime = await showTimePicker(
       context: context,
@@ -41,12 +52,68 @@ class _LoginState extends State<Login> {
     );
     if (newTime != null) {
       setState(() {
-        var result = "${newTime.hour}:${newTime.minute}";
-        time = result;
-        controllerWaktu.text = result;
+        timeAwal = newTime.toString().split('(')[1].substring(0, 5);
+        controllerWaktuAwal.text = timeAwal;
       });
     }
-    print(time);
+    print(timeAwal);
+  }
+
+  var timeAkhir;
+  void selectTimeAkhir() async {
+    final TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (newTime != null) {
+      setState(() {
+        timeAkhir = newTime.toString().split('(')[1].substring(0, 5);
+        controllerWaktuAkhir.text = timeAkhir;
+      });
+    }
+    print(timeAkhir);
+  }
+
+  var a = [
+    {
+      "waktu": "10:00",
+      "waktuL": "10:10",
+    },
+    {
+      "waktu": "11:00",
+      "waktuL": "11:10",
+    },
+  ];
+  @override
+  void initState() {
+    super.initState();
+    DateTime date = DateTime.now();
+    final time = TimeOfDay.now();
+    TimeOfDay time1 = TimeOfDay(hour: 09, minute: 10);
+    TimeOfDay time2 = TimeOfDay(hour: 01, minute: 20);
+    a.forEach((element) { 
+      var b= element['waktu'] == "10:00";
+      print(b);
+      print(element);
+    });
+    // print(a.singleWhere((value) => value['waktu'] == "11:00"));
+    // if (time.hour.clamp(14, 15) == time.hour && time.minute.clamp(00, 01) == time.minute) {
+    //   print('jam');
+    // } else {
+    //   print('bukan');
+    // }
+    // final startTime = DateTime.now();
+    // final endTime = DateTime(2018, 6, 23, 16, 00);
+    // final currentTime = DateTime.now();
+
+    // if (currentTime.isAfter(startTime) && currentTime.isBefore(endTime)) {
+    //   print('ada');
+    // } else {
+    //   print('ga');
+    // }
+    // print(time.toString().split('(')[1].substring(0, 5));
+    // print(time);
+    // print(format.format(date));
   }
 
   @override
@@ -68,8 +135,7 @@ class _LoginState extends State<Login> {
                       TextFormField(
                         controller: controllerTerminal,
                         keyboardType: TextInputType.number,
-                        decoration: const InputDecoration(
-                            hintText: 'Masukan Nomor Terminal'),
+                        decoration: const InputDecoration(hintText: 'Masukan Nomor Terminal'),
                         validator: (value) {
                           if (value == null || value.isEmpty) {
                             return "Kolom ini tidak boleh kosong";
@@ -82,9 +148,23 @@ class _LoginState extends State<Login> {
                         },
                         child: TextFormField(
                           enabled: false,
-                          controller: controllerWaktu,
-                          decoration:
-                              const InputDecoration(hintText: 'Masukan Jam'),
+                          controller: controllerWaktuAwal,
+                          decoration: const InputDecoration(hintText: 'Masukan Jam Awal'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Kolom ini tidak boleh kosong";
+                            }
+                          },
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          selectTimeAkhir();
+                        },
+                        child: TextFormField(
+                          enabled: false,
+                          controller: controllerWaktuAkhir,
+                          decoration: const InputDecoration(hintText: 'Masukan Jam Akhir'),
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return "Kolom ini tidak boleh kosong";
@@ -139,13 +219,7 @@ class _LoginState extends State<Login> {
             padding: const EdgeInsets.all(8.0),
             child: ElevatedButton(
               onPressed: () async {
-                // submit();
-                list.add({
-                  "terminal": controllerTerminal.text,
-                  "status": status,
-                });
-                SharedPreferences pref = await SharedPreferences.getInstance();
-                pref.setString('setting', jsonEncode(list));
+                submit();
               },
               child: Text('SUBMIT'),
             ),
@@ -155,11 +229,20 @@ class _LoginState extends State<Login> {
             child: ElevatedButton(
               onPressed: () async {
                 // submit();
+                // print(controllerWaktuAkhir.text.toString().split(':')[1]);
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => Masuk()));
                 // print(list);
-                SharedPreferences pref = await SharedPreferences.getInstance();
-                var a = pref.getString('setting');
-                 var json = jsonDecode(a.toString());
-                print(json[0]);
+                // SharedPreferences pref = await SharedPreferences.getInstance();
+                // var a = pref.getString('setting');
+                // var json = jsonDecode(a.toString());
+                // data = json;
+                // var result = data.takeWhile((value) => value['waktu'] == "08:40");
+                // // print();
+                // result.forEach((element) {
+                //   print(element);
+                // });
+                // // print(json);
+                // print("data $data");
               },
               child: Text('SUBMIT'),
             ),
--- lib/source/masuk.dart
+++ lib/source/masuk.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
+import 'dart:ui';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
@@ -26,21 +27,14 @@ class _MasukState extends State<Masuk> {
 
   int isMasuk = 0;
 
-  var list = [
-    {
-      "name": "raga2",
-      "status": 0,
-    },
-    {
-      "name": "ragaaa",
-      "status": 1,
-    },
-  ];
-
   AudioPlayer player = AudioPlayer();
   late Uint8List audiobytes;
 
+  var loading = true;
+  // var setting = [];
+  var list = {};
   var data = {};
+  var array = [];
 
   void masuk(id) async {
     print('masuk');
@@ -59,20 +53,20 @@ class _MasukState extends State<Masuk> {
                 var jsonLast = jsonDecode(value.body);
                 print(jsonLast);
                 if (jsonLast is List) {
-                  setState(() {
-                    data = jsonLast[0];
-                    Timer(Duration(seconds: 3), () {
+                  Timer(Duration(seconds: 3), () {
+                    setState(() {
+                      data = jsonLast[0];
                       data.clear();
+                      controller.clear();
                     });
-                    controller.clear();
                   });
                 } else {
-                  setState(() {
-                    data = jsonLast;
-                    Timer(Duration(seconds: 3), () {
+                  Timer(Duration(seconds: 3), () {
+                    setState(() {
+                      data = jsonLast;
                       data.clear();
+                      controller.clear();
                     });
-                    controller.clear();
                   });
                 }
                 print("Data: $data");
@@ -100,16 +94,66 @@ class _MasukState extends State<Masuk> {
     });
   }
 
+  void getSetting() async {
+    DateTime date = DateTime.now();
+    TimeOfDay time = TimeOfDay.now();
+    var format = time.toString().split('(')[1].substring(0, 5);
+    var jam = format.toString().split(':')[0];
+    var menit = format.toString().split(':')[1];
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    var settings = pref.getString('setting');
+    var json = jsonDecode(settings.toString());
+    setState(() {
+      array = json;
+    });
+    print(format);
+    print('JSON : $json');
+    if (settings != null) {
+      loading = false;
+      // var result = json.singleWhere((value) => value['waktuAwal'] == format.toString());
+      json.forEach((element) {
+        var hasiljam1 = element['waktuAwal'].toString().split(':')[0];
+        var hasilmenit1 = element['waktuAwal'].toString().split(':')[1] == 00 ? 00 : element['waktuAwal'].toString().split(':')[1];
+        var hasiljam2 = element['waktuAkhir'].toString().split(':')[0];
+        var hasilmenit2 = element['waktuAkhir'].toString().split(':')[1] == 00 ? 00 : element['waktuAkhir'].toString().split(':')[1];
+        print("Element : $element");
+        if (time.hour.clamp(int.parse(hasiljam1), int.parse(hasiljam2)) == time.hour &&
+            time.hour.clamp(int.parse(hasilmenit1.toString()), int.parse(hasilmenit2.toString())) == time.minute) {
+          setState(() {
+            list = element;
+          });
+          print('ada');
+        } else {
+          print("Kosong menit");
+          setState(() {
+            list.clear();
+          });
+        }
+      });
+
+      // }
+      // SharedPreferences pref = await SharedPreferences.getInstance();
+      // pref.setString("savesSetting", list.toString());
+      print('LIST : $list');
+      // });
+    }
+  }
+
+  void getSaveSetting() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    var settings = pref.getString('setting');
+    var save = pref.getString("savesSetting");
+    var json = jsonDecode(settings.toString());
+  }
+
   @override
   void initState() {
     super.initState();
     // play();
     // masuk();
-    DateTime date = DateTime.now();
-    String format = DateFormat('hh:mm').format(date);
-    print(format);
-    var result = list.takeWhile((value) => value['name'] == "raga2");
-    print(result);
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      getSetting();
+    });
   }
 
   @override
@@ -119,12 +163,11 @@ class _MasukState extends State<Masuk> {
       appBar: AppBar(
         elevation: 0.0,
         centerTitle: true,
-        title: const Text("MASUK",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                fontWeight: FontWeight.w600)),
+        title: list.isEmpty
+            ? const Text("", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.w600))
+            : list['status'] == 0
+                ? const Text("MASUK", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.w600))
+                : const Text("KELUAR", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.w600)),
         actions: [
           IconButton(
             onPressed: () {
@@ -136,83 +179,83 @@ class _MasukState extends State<Masuk> {
       ),
       body: ListView(
         children: [
-          Container(
-            margin: const EdgeInsets.all(8.0),
-            padding: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-              color: Colors.green,
-              borderRadius: BorderRadius.circular(12.0),
-            ),
-            height: 400,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: controller,
-                  style: const TextStyle(color: Colors.white),
-                  cursorColor: Colors.green,
-                  decoration: const InputDecoration(
-                    fillColor: Colors.green,
-                    border: InputBorder.none,
+          loading
+              ? const SizedBox(
+                  height: 400,
+                  child: Center(
+                    child: CircularProgressIndicator(),
                   ),
-                  onChanged: (value) async {
-                    if (value.length >= 9) {
-                      await Future.delayed(Duration(milliseconds: 650));
-                      masuk(controller.text);
-                    }
-                  },
-                ),
-                data.isNotEmpty
-                    ? Column(
+                )
+              : list.isEmpty
+                  ? SizedBox(
+                      height: 400,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [Text('Sistem Offline', style: TextStyle(color: Colors.white, fontSize: 25))],
+                      ))
+                  : Container(
+                      margin: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: list['status'] == 0 ? Colors.green : Colors.red[700],
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      height: 400,
+                      child: Column(
                         children: [
-                          Text(
-                            data['barcode'].toString() == 'null'
-                                ? ''
-                                : data['barcode'].toString(),
-                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          TextFormField(
+                            controller: controller,
+                            style: const TextStyle(color: Colors.white),
+                            cursorColor: Colors.green,
+                            decoration: const InputDecoration(
+                              fillColor: Colors.green,
+                              border: InputBorder.none,
+                            ),
+                            onChanged: (value) async {
+                              if (value.length >= 9) {
+                                await Future.delayed(Duration(milliseconds: 650));
+                                masuk(controller.text);
+                              }
+                            },
                           ),
-                          Text(
-                            data['FullName'].toString() == 'null'
-                                ? ''
-                                : data['FullName'].toString(),
-                            style: TextStyle(color: Colors.white, fontSize: 30),
-                          ),
-                          Text(
-                            data['Tanggal'].toString() == 'null'
-                                ? ''
-                                : data['Tanggal'].toString(),
-                            style: TextStyle(color: Colors.white, fontSize: 30),
-                          ),
-                          Text(
-                            data['waktu'].toString() == 'null'
-                                ? ''
-                                : data['waktu'].toString(),
-                            style: TextStyle(color: Colors.white, fontSize: 30),
-                          ),
-                          Text(
-                            data['status'].toString() == 'null'
-                                ? ''
-                                : data['status'].toString(),
-                            style: TextStyle(color: Colors.white, fontSize: 30),
-                          ),
+                          data.isNotEmpty
+                              ? Column(
+                                  children: [
+                                    Text(
+                                      data['barcode'].toString() == 'null' ? '' : data['barcode'].toString(),
+                                      style: TextStyle(color: Colors.white, fontSize: 30),
+                                    ),
+                                    Text(
+                                      data['FullName'].toString() == 'null' ? '' : data['FullName'].toString(),
+                                      style: TextStyle(color: Colors.white, fontSize: 30),
+                                    ),
+                                    Text(
+                                      data['Tanggal'].toString() == 'null' ? '' : data['Tanggal'].toString(),
+                                      style: TextStyle(color: Colors.white, fontSize: 30),
+                                    ),
+                                    Text(
+                                      data['waktu'].toString() == 'null' ? '' : data['waktu'].toString(),
+                                      style: TextStyle(color: Colors.white, fontSize: 30),
+                                    ),
+                                    Text(
+                                      data['status'].toString() == 'null' ? '' : data['status'].toString(),
+                                      style: TextStyle(color: Colors.white, fontSize: 30),
+                                    ),
+                                  ],
+                                )
+                              : Container()
                         ],
-                      )
-                    : Container()
-              ],
-            ),
-          ),
+                      ),
+                    ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: DigitalClock(
               is24HourTimeFormat: true,
               areaDecoration: const BoxDecoration(color: Colors.transparent),
-              hourMinuteDigitTextStyle:
-                  const TextStyle(color: Colors.white, fontSize: 50),
-              hourMinuteDigitDecoration:
-                  const BoxDecoration(color: Colors.transparent),
-              secondDigitDecoration:
-                  const BoxDecoration(color: Colors.transparent),
-              secondDigitTextStyle:
-                  const TextStyle(color: Colors.white, fontSize: 50),
+              hourMinuteDigitTextStyle: const TextStyle(color: Colors.white, fontSize: 50),
+              hourMinuteDigitDecoration: const BoxDecoration(color: Colors.transparent),
+              secondDigitDecoration: const BoxDecoration(color: Colors.transparent),
+              secondDigitTextStyle: const TextStyle(color: Colors.white, fontSize: 50),
             ),
           ),
         ],
@@ -222,8 +265,7 @@ class _MasukState extends State<Masuk> {
 
   void play() async {
     ByteData bytes = await rootBundle.load("assets/Announce.mp3");
-    audiobytes =
-        bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
+    audiobytes = bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
     await player.playBytes(audiobytes);
   }
 
@@ -287,8 +329,7 @@ class _MasukState extends State<Masuk> {
   }
 
   void setup() async {
-    if (controllerUsername.text != 'admin' &&
-        controllerPassword.text != 'admin') {
+    if (controllerUsername.text != 'admin' && controllerPassword.text != 'admin') {
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -311,8 +352,7 @@ class _MasukState extends State<Masuk> {
     } else {
       SharedPreferences pref = await SharedPreferences.getInstance();
       pref.clear();
-      Navigator.pushAndRemoveUntil(context,
-          MaterialPageRoute(builder: (_) => const Login()), (route) => false);
+      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (_) => const Login()), (route) => false);
     }
   }
 
@@ -327,25 +367,16 @@ class _MasukState extends State<Masuk> {
             children: [
               Text(
                 'STOP !',
-                style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.yellow),
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600, color: Colors.yellow),
               ),
               Divider(color: Colors.yellow, thickness: 5),
               Text(
                 'KEPADA $nama',
-                style: TextStyle(
-                    fontSize: 23,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.yellow),
+                style: TextStyle(fontSize: 23, fontWeight: FontWeight.w600, color: Colors.yellow),
               ),
               Text(
                 'MOHON HUBUNGI POLIKLINIK PERUSAHAAN ATAU SATPAM',
-                style: TextStyle(
-                    fontSize: 33,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white),
+                style: TextStyle(fontSize: 33, fontWeight: FontWeight.w600, color: Colors.white),
               ),
             ],
           ),
